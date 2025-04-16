964421e0631c582f655186ea6a485a060494f17a
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2025-04-16 16:21:23 +0300

    Add GH Actions autodeploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://maiger.github.io",
+  base: "/cyperpunk-portfolio",
   integrations: [icon()],
 });
